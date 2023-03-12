290fa12d1b5d4c1ea43f6a8319e333f31603578c
Felipe Curty <felipecrp@gmail.com>
2023-03-12 01:39:51 -0400

    Add real tests

--- test/test_sample.py
+++ test/test_sample.py
@@ -1,31 +1,31 @@
-import pytest
+# import pytest
 
-class TestHouse:
-    "a House"
+# class TestHouse:
+#     "a House"
     
-    def test_door(self):
-        "has door"
-        assert 1 == 1
+#     def test_door(self):
+#         "has door"
+#         assert 1 == 1
         
-    class TestTwoFloors:
-        """with two floors
+#     class TestTwoFloors:
+#         """with two floors
         
-        A house with two floor has stairs
-        """
-        def test_stairs(self):
-            "has stairs"
-            assert 1 == 1
+#         A house with two floor has stairs
+#         """
+#         def test_stairs(self):
+#             "has stairs"
+#             assert 1 == 1
 
-        def it_has_second_floor(self):
-            assert 1 == 1
+#         def it_has_second_floor(self):
+#             assert 1 == 1
 
-        @pytest.mark.xfail
-        def test_third_floor(self):
-            "has third floor"
-            assert 1 == 2
+#         @pytest.mark.xfail
+#         def test_third_floor(self):
+#             "has third floor"
+#             assert 1 == 2
 
-class describe_a_great_house:
-    class with_three_floors:
-        def it_has_multiple_stairs(self):
-            assert 1 == 1
+# class describe_a_great_house:
+#     class with_three_floors:
+#         def it_has_multiple_stairs(self):
+#             assert 1 == 1
         
--- test/test_test.py
+++ test/test_test.py
@@ -1,18 +1,77 @@
+import re
+
 pytest_plugins = ["pytester"]
 
 class Test:
     "A Test" 
 
     class with_docstring:
+        "with docstring"
 
         def test_docstring(self, pytester):
+            "shows the docstring"
             pytester.makepyfile("""
                 def test_hello():
+                    '''say hello'''
                     assert 1 == 1
             """)
 
             result = pytester.runpytest()
-            assert 1 == 1
+            output = '\n'.join(result.outlines)
+            assert re.search(r'✓ Say hello', output)
 
-def test_hello():
-    assert 1 == 1
+    class with_multiline_docstring:
+        "with multiline docstring"
+
+        def test_docstring(self, pytester):
+            "shows the first line of the docstring"
+            pytester.makepyfile("""
+                def test_hello():
+                    '''say hello
+                    hidden text'''
+                    assert 1 == 1
+            """)
+
+            result = pytester.runpytest()
+            output = '\n'.join(result.outlines)
+            assert re.search(r'✓ Say hello', output)
+            assert not re.search(r'hidden text', output)
+
+class TestCase:
+    "A test case"
+
+    class with_docstring:
+        "with docstring"
+
+        def test_docstring(self, pytester):
+            "shows the docstring"
+            pytester.makepyfile("""
+                class TestHello:
+                    '''A hello'''
+                    def test_hello(self):
+                        '''say hello'''
+                        assert 1 == 1
+            """)
+
+            result = pytester.runpytest()
+            output = '\n'.join(result.outlines)
+            assert re.search(r'A hello\n  ✓ Say hello', output)
+
+    class with_multiline_docstring:
+        "with multiline docstring"
+
+        def test_docstring(self, pytester):
+            "shows the first line of the docstring"
+            pytester.makepyfile("""
+                class TestHello:
+                    '''A hello
+                    hidden text'''
+                    def test_hello(self):
+                        '''say hello'''
+                        assert 1 == 1
+            """)
+
+            result = pytester.runpytest()
+            output = '\n'.join(result.outlines)
+            assert re.search(r'A hello\n  ✓ Say hello', output)
+            assert not re.search(r'hidden text', output)
